757df6605eb7fdaa92faf9f1c59a0208594584f0
Hyder <mafzal@bulir.com>
2025-08-14 11:45:48 +0500

    server url added in env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
-  baseUrl: 'https://localhost:5001/api',
-  // baseUrl: 'https://visualize-api.connektahub.com/api',
-  connecktaApp: 'https://dev-v2.connektahub.xyz/app',
+  // baseUrl: 'https://localhost:5001/api',
+  baseUrl: 'https://x-api.connekta.au/api',
+  connecktaApp: 'https://app.connekta.au/app',
   conneckta: {
     tenantLogin: 'https://app.connekta.au/app/login',
-    apiBaseUrl: 'https://dev-v2-api.connektahub.xyz/api',
+    apiBaseUrl: 'https://api.voxie.site/api',
   },
 };
